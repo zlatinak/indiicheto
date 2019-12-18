1fc8ac97daa53b29b2507ab7e209a832398cf0f8
zlatinap <zlatinap@safecharge.com>
2019-12-18 09:52:51 +0200

    handle alerts

--- src/test/java/Assigment.java
+++ src/test/java/Assigment.java
@@ -1,8 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assigment {
 
     ///Lesson 62
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException {
 
+        String text = "Zlatina";
 
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\zlatinap\\Desktop\\Selenium_Drivers\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.findElement(By.id("name")).sendKeys(text); //enter text value in name field
+        driver.findElement(By.cssSelector("#alertbtn")).click();//click on Alert button
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();//switching to alert window and press ok
+        driver.findElement(By.cssSelector("#confirmbtn")).click();//click on Confirm button
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();// switch to alert window and press cancel
+
+        Thread.sleep(2000);
+        driver.close();
     }
 }
