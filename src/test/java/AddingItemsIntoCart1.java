eaa551359345e0f79ff33d7e0f3278764034ffbf
zlatinap <zlatinap@safecharge.com>
2020-01-02 12:00:14 +0200

    Change the way clicking on "Add to cart" button adding counter to break the "for" split a String

--- src/test/java/AddingItemsIntoCart1.java
+++ src/test/java/AddingItemsIntoCart1.java
@@ -21,29 +21,42 @@ import java.util.List;
 
 public class AddingItemsIntoCart1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Zlatina\\Desktop\\Selenium_Drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\zlatinap\\Desktop\\Selenium_Drivers\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
-        String[] itemsNeeded = {"Cucumber", "Brokoli","Beetroot"};//array in JAVA
-
+        String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot"};//array in JAVA
+        int j=0;//counter how many veggi were found from the itemsNeeded
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+
 
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));// all products with common cssSelector are stored
 
         for (int i = 0; i < products.size(); i++) {
 
-            String name = products.get(i).getText(); // Store text of the current Element
-
+//            String name = products.get(i).getText(); // Store text of the current Element
+            String[] name = products.get(i).getText().split("-");//it will be split on two elements - before and after the "-"
+            name[0].trim();//will trim empty spaces
+            String formattedName = name[0].trim();
             //convert array into array list for easy search
             //check whether name your extract is present in array or not
             List itemsNeededList = Arrays.asList(itemsNeeded);
-            System.out.println(name);
-            if (itemsNeededList.contains(name)) {   //check whether this text contains Cucumber
-                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+            System.out.println(formattedName);
+
+            if (itemsNeededList.contains(formattedName)) {   //check whether this text contains Cucumber
+                j++;
+                //click on Add to cart
+                driver.findElements(By.cssSelector(".product-action")).get(i).click();
+                System.out.println("click");
+                if(j==itemsNeeded.length) {
+                    break;
+                }
+                }
 
             }
-        }
+
 
     }
 }
